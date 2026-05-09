6487be0261ead0a409489c96d4fb09a113e46055
Cody Winter <win24054@byui.edu>
2026-05-09 15:52:06 -0600

    made list and method for promptgenerator

---
+++ journal/promptgenerator.cs
@@ -0,0 +1,16 @@
+using system;
+
+public class promptgenerator
+{
+    List<string> Prompts = new List<string>()
+    {
+        "What was the highlight of your day?",
+        "what was the low-ligt of your day?",
+        "What was the hardest part of your day?",
+        "What friends or people did you talk to?",
+    };
+    string GetRandomPrompt()
+    {
+        Random random = new Random();
+    }
+}
