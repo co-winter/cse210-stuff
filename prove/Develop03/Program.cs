d7c87c3c64598f1d3c9a23dac460afd16b1bb5ec
Cody Winter <win24054@byui.edu>
2026-05-25 21:46:17 -0600

    rough draf of program class

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        reference reference = new reference("Proverbs", "3", 5, 6);
+
+        string text = "Trust in the Lord with all thine heart and lean not unto thine own understanding in all thy ways acknowledge him and he shall direct thy paths";
+
+        scripture scripture = new scripture(reference, text);
+
+        bool continueProgram = true;
+
+        while (continueProgram)
+        {
+            Console.Clear();
+
+            scripture.DisplayScripture();
+
+            if (scripture.AllWordsHidden())
+            {
+                continueProgram = false;
+                break;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press ENTER to continue or type quit");
+
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                continueProgram = false;
+            }
+            else
+            {
+                scripture.HideWords();
+            }
+        }
     }
 }
