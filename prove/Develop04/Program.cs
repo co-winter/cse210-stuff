7604ff6df32ef8446d3f973b830564dcdd41a7cf
Cody Winter <win24054@byui.edu>
2026-06-10 22:17:51 -0600

    program draft

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        string choice = "";
+
+        while (choice != "4")
+        {
+            Console.Clear();
+
+            Console.WriteLine("Mindfulness Program");
+            Console.WriteLine();
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflection Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Quit");
+            Console.WriteLine();
+
+            Console.Write("Select a choice: ");
+            choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Breathing breathing = new Breathing();
+                breathing.RunActivity();
+            }
+            else if (choice == "2")
+            {
+                Reflection reflection = new Reflection();
+                reflection.RunActivity();
+            }
+            else if (choice == "3")
+            {
+                Listing listing = new Listing();
+                listing.RunActivity();
+            }
+        }
     }
 }
