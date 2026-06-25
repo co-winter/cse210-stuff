8d35d3d6305eaa5a8e2ac2545c0dfd8419d9eed3
Cody Winter <win24054@byui.edu>
2026-06-25 12:16:50 -0600

    program draft

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        GoalManager manager = new GoalManager();
+        string choice = "";
+
+        while (choice != "6")
+        {
+            Console.Clear();
+            Console.WriteLine($"You have {manager.GetScore()} points.");
+            Console.WriteLine();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("  1. Create New Goal");
+            Console.WriteLine("  2. List Goals");
+            Console.WriteLine("  3. Save Goals");
+            Console.WriteLine("  4. Load Goals");
+            Console.WriteLine("  5. Record Event");
+            Console.WriteLine("  6. Quit");
+            Console.Write("Select a choice from the menu: ");
+            choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                manager.CreateGoal();
+            }
+            else if (choice == "2")
+            {
+                manager.ListGoals();
+            }
+            else if (choice == "3")
+            {
+                Console.Write("Enter filename to save to: ");
+                string filename = Console.ReadLine();
+                manager.SaveGoals(filename);
+            }
+            else if (choice == "4")
+            {
+                Console.Write("Enter filename to load from: ");
+                string filename = Console.ReadLine();
+                manager.LoadGoals(filename);
+            }
+            else if (choice == "5")
+            {
+                manager.RecordEvent();
+            }
+        }
     }
 }
